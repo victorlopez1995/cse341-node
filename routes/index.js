064614b3904d6761a6bdb1d49ce3ea300afcec7c
victorlopez1995 <lop22010@byui.edu>
2023-01-13 21:47:28 -0500

    create swagger and api doc

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use('/', require('./swagger'));
+
 // Home page route
 router.use("/contacts", require("./contacts"));
 
